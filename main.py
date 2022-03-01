67d949b2acd6007b28f53f53eda313a9c876dff8
Valesz <szvalesz98@gmail.com>
2022-03-01 17:01:33 +0100

    Pridanie

---
+++ main.py
@@ -0,0 +1,12 @@
+import socket
+import socketserver
+
+import sipfullproxy
+
+if __name__ == '__main__':
+    hostname = socket.gethostname()
+    ipaddress = socket.gethostbyname(hostname)
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress, sipfullproxy.PORT)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress, sipfullproxy.PORT)
+    server = socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
+    server.serve_forever()
